40b05f742b09bd233def85f0f44b73730d8457db
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2021-05-22 20:57:10 +0200

    wip: still need to fix terminals not showing up

--- src/grammar-inducer.ts
+++ src/grammar-inducer.ts
@@ -14,15 +14,35 @@ export type SExpression = string | Array<SExpression>;
 export function addToRules(rule: { lhs: string; rhs: Array<SExpression> }): boolean {
   // TODO: implement it correctly
   // Look data structure that u want to use (u wrote on a paper)
-  console.log(rule)
+  // console.log(rule)
   if (!rules[rule.lhs]) {
     rules[rule.lhs] = {}
   }
   rule.rhs.forEach(r => {
-    rules[rule.lhs][(r as string)] = {count: 1, weight: 1}
-  })
-  // rules[rule.lhs] = rule.rhs
+    // TODO: use regex!!!
+    const regex = /([^,"\[\]]+)/gm
+    const str = JSON.stringify(r).match(regex)
+    let finalText = ''
+    if (str.length > 1) {
+      // TODO: rename variables x, str and finalText
+      // TODO: see if there is a better implementation
+      let x = ''
+      for (let i = 0; i < str?.length; i++) {
+        // if(i === x.length - 1) {}
+        x = x + str[i] + ' '
+      }
+      // If last character is a whitespace, then remove it.
+      if (x[x.length - 1] === ' ') {
+        finalText = (x as string).slice(0, -1)
+      } else {
+        finalText = (x as string)
+      }
+    }
 
+    rules[rule.lhs][(finalText as string)] = {count: 1, weight: 1}
+  })
+  // TODO: see if it makes sense to return a boolean
+  // if thats the case, then fix the returned value
   return true
 }
 
@@ -57,7 +77,7 @@ export function extractRules(exp: Array<SExpression>): void {
 export function runMyApp(): void {
   const exp: SExpression = ['S', ['NP', 'John'], ['VP', ['V', 'hit'], ['NP', ['DET', 'the'], ['N', 'ball']]]]
   extractRules(exp)
-  console.log('rules = ')
+  // console.log('rules = ')
   console.log(JSON.stringify(rules))
 }
 runMyApp()
