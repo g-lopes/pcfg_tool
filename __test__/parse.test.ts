3cbf575364496bad9fcbd6d4b0ee8079ccf9b4ae
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2021-06-10 19:46:00 +0200

    add tests for ckyChartWeight with unary rules

--- __test__/parse.test.ts
+++ __test__/parse.test.ts
@@ -148,7 +148,6 @@ test('initializeWeightChart with unary rules', () => {
   }
 })
 
-//
 test('ckyChartBoolean', () => {
   const rulesFilePath = path.join(__dirname, './data/cnf_grammar.rules')
   const lexiconFilePath = path.join(__dirname, './data/cnf_grammar.lexicon')
@@ -164,7 +163,7 @@ test('ckyChartBoolean', () => {
   expect(chart).toEqual(expectedChart)
 })
 
-test('ckyChartWeight', () => {
+test('ckyChartWeight wihout unary rules', () => {
   const rulesFilePath = path.join(__dirname, './data/cnf2_grammar.rules')
   const lexiconFilePath = path.join(__dirname, './data/cnf2_grammar.lexicon')
   const sentence = 'the man saw the dog'
@@ -185,27 +184,24 @@ test('ckyChartWeight', () => {
   }
 })
 
-// test('ckyChartWeight with unary rules', () => {
-//   const rulesFilePath = path.join(__dirname, './data/simple_pcfg3.rules')
-//   const lexiconFilePath = path.join(__dirname, './data/simple_pcfg3.lexicon')
-//   const sentence = 'he saw her duck'
-//   const chart: WeightChart = ckyChartWeight(sentence, lexiconFilePath, rulesFilePath)
-//   const expectedChart: WeightChart = [
-//     [{}, {DT: 1}, {NP: 0.08}, {}, {}, {S: 0.0256}],
-//     [{}, {}, {NN: 0.1}, {}, {}, {}],
-//     [{}, {}, {}, {Vt: 1}, {}, {VP: 0.32}],
-//     [{}, {}, {}, {}, {DT: 1}, {NP: 0.4}],
-//     [{}, {}, {}, {}, {}, {NN: 0.5}],
-//   ]
-//   for (let i = 0; i < chart.length; i++) {
-//     for (let j = 0; j < chart[i].length; j++) {
-//       for (const [key] of Object.entries(chart[i][j])) {
-//         console.log(key)
-//         expect(chart[i][j][key]).toBeCloseTo(expectedChart[i][j][key])
-//       }
-//     }
-//   }
-// })
+test('ckyChartWeight with unary rules', () => {
+  const rulesFilePath = path.join(__dirname, './data/cnf_with_unary.rules')
+  const lexiconFilePath = path.join(__dirname, './data/cnf_with_unary.lexicon')
+  const sentence = 'lead can poison'
+  const chart: WeightChart = ckyChartWeight(sentence, lexiconFilePath, rulesFilePath)
+  const expectedChart: WeightChart = [
+    [{}, {N: 0.3, V: 0.9, NP: 0.3 * 0.7, VP: 0.9 * 0.3}, {NP: 0.6 * 0.3 * 0.14}, {S: 1 * 0.0252 * 0.03, NP: 0.6 * 0.3 * 0.042}],
+    [{}, {}, {N: 0.2, M: 0.5, NP: 0.7 * 0.2}, {S: 1 * 0.14 * 0.03, VP: 0.4 * 0.5 * 0.1, NP: 0.6 * 0.2 * 0.35}],
+    [{}, {}, {}, {N: 0.5, V: 0.1, NP: 0.7 * 0.5, VP: 0.3 * 0.1}],
+  ]
+  for (let i = 0; i < chart.length; i++) {
+    for (let j = 0; j < chart[i].length; j++) {
+      for (const [key] of Object.entries(chart[i][j])) {
+        expect(chart[i][j][key]).toBeCloseTo(expectedChart[i][j][key])
+      }
+    }
+  }
+})
 
 test('getAllUnaryProductionsFromRulesFile', () => {
   const rulesFilePath = path.join(__dirname, './data/simple_pcfg.rules')
@@ -219,9 +215,3 @@ test('getAllUnaryProductionsFromRulesFile', () => {
   ]
   expect(unaryRules).toEqual(expectedUnaryRules)
 })
-
-// test('unaryClosure', () => {
-//   const closure = unaryClosure()
-//   const expectedClosure: Set<Production> = new Set()
-//   expect(closure).toBe(expectedClosure)
-// })
